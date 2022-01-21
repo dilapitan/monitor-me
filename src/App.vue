f2593c75bfe079ea527fac0f6965e024e7b4bfa5
dilapitan <domlapitan@gmail.com>
2022-01-21 22:39:15 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title> Monitor Me! </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/dilapitan/monitor-me"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Source Code</span>
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main> </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {},
+
+  data: () => ({
+    //
+  })
+}
+</script>
